e68c3a11ce4f3fcdbf4f28f1c1c80b392fcebd59
Scott Tolman <scotttolman@gmail.com>
2018-03-08 23:55:42 -0600

    added transform css

---
+++ css/main.js
@@ -0,0 +1,25 @@
+function _translate() {
+    var styl = document.getElementById("_translate");
+    if (!styl)
+        styl.style.transform = "translate(20px, -20px)";
+    else
+        styl.style.transform = null;
+}
+
+function _rotate() {
+}
+
+function _scale() {
+}
+
+function _skewX() {
+}
+
+function _skewY() {
+}
+
+function _skew() {
+}
+
+function _matrix() {
+}
