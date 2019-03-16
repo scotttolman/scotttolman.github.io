27773c502f8ebe44eecc2f3c02fa716efb115d18
scott <scotttolman@gmail.com>
2019-03-16 14:57:54 -0500

    adding CS371 project

---
+++ Project/browse.js
@@ -0,0 +1,20 @@
+function home() {
+    alert("This would link to the home page")
+}
+
+function selectAllAudience() {
+    alert("All Audience selected")
+}
+
+function selectAllGenre() {
+    alert("All Genre selected")
+}
+
+function eBooks() {
+    alert("E-Books toggled")
+}
+
+function sortBy() {
+    let value = document.getElementById("sort-by").value
+    alert("Sort by " + value + " was selected")
+}
